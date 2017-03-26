bbede3b329bc3bec94637c695aa47c5262f59ae6
duy.vo <duy.vo@pyramid-consulting.com>
2017-03-26 20:02:13 +0700

    Done chapter 5

--- src/Chapter5.hs
+++ src/Chapter5.hs
@@ -29,3 +29,109 @@ palindromeRearranging (x: xs) =
     if x `elem` xs then palindromeRearranging(delete x xs)
     else (rem (length xs) 2 == 0) && palindromeRearranging xs
 
+tryFun :: a -> a -> a
+tryFun x y = y
+--tryFun x y = x
+
+tryFun2 :: a -> b -> b
+tryFun2 x y = y
+
+functionH :: [a] -> a
+functionH (x:_) = x
+
+functionC :: Ord a => a -> a -> Bool
+functionC x y = if x > y then True else False -- should be x > y
+
+functionS :: (a, b) -> b
+functionS (x, y) = y
+
+id :: a -> a
+id x = x
+
+c :: a -> b -> a
+c x y = x
+
+c'' :: b -> a -> b
+c'' x y = x
+
+c' :: a -> b -> b
+c' x y = y
+
+r :: [a] -> [a]
+--r x = tail x
+--r x = init x
+r x = x ++ x
+
+co :: (b -> c) -> (a -> b) -> (a -> c)
+co f g = f . g
+
+a:: (a -> c) -> a -> a
+a f x = x
+
+a' :: (a -> b) -> a -> b
+a' f x = f x
+
+fstString :: String -> String
+fstString x = x ++ " in the rain"
+
+sndString :: String -> String
+sndString x = x ++ " over the rainbow"
+
+sing = if x < y then fstString x else sndString y
+  where x = "Singin"; y = "Somewhere"
+
+
+main :: IO ()
+main = do
+  print (1 + 2)
+  print blah
+  print $ negate (-1)
+  print $ (+) 0 blah
+  where blah = negate 1
+
+data Woot
+data Blah
+
+f1 :: Woot -> Blah
+f1 = undefined
+
+g1 :: (Blah, Woot) -> (Blah, Blah)
+g1 (b, w) = (b, f1 w)
+
+f2 :: Int -> String
+f2 = undefined
+
+g2 :: String -> Char
+g2 = undefined
+
+h2:: Int -> Char
+h2 x = g2 $ f2 x
+
+data A
+data B
+data C
+
+q :: A -> B
+q = undefined
+
+w :: B -> C
+w = undefined
+
+e :: A -> C
+e x = w $ q x
+
+data X
+data Y
+data Z
+
+xz :: X -> Z
+xz = undefined
+
+yz :: Y -> Z
+yz = undefined
+
+xform :: (X, Y) -> (Z, Z)
+xform (x, y) = (xz x, yz y)
+
+munge :: (x -> y) -> (y -> (w, z)) -> x -> w
+munge f g x = fst $ g $ f x
